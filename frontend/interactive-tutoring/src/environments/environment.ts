ee1b0d7219db0762bb13116fc43d0f7efa1e8a58
m-gibas <gibasmaksymilian@gmail.com>
2023-10-22 19:49:44 +0200

    version change to be compatible with ngx-socket-io

--- frontend/interactive-tutoring/src/environments/environment.ts
+++ frontend/interactive-tutoring/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080',
-  //   socketUrl: 'http://localhost:8085'
-  socketUrl: 'ws://localhost:8085'
+  socketUrl: 'http://localhost:8085'
 };
