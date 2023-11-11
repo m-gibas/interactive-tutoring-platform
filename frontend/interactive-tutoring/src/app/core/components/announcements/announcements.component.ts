44794e3a7546b24ba60168b97e99262060ec28a8
Maksymilian Gibas <104219943+m-gibas@users.noreply.github.com>
2023-11-12 00:03:16 +0100

    sort merhods style and logic update

--- frontend/interactive-tutoring/src/app/core/components/announcements/announcements.component.ts
+++ frontend/interactive-tutoring/src/app/core/components/announcements/announcements.component.ts
@@ -3,7 +3,15 @@ import { CommonModule, DatePipe, NgFor } from '@angular/common';
 import { UserService } from '../../services/user.service';
 import { AddAnnouncementComponent } from './add-announcement/add-announcement.component';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { Observable, map } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  switchMap
+} from 'rxjs';
 import {
   Announcement,
   SortType,
@@ -14,7 +22,7 @@ import { SortByPipe } from '../../pipes/sort-by.pipe';
 import { MultiSelectComponent } from './multi-select/multi-select.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material/core';
+import { DateAdapter, MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 
@@ -44,84 +52,71 @@ export class AnnouncementsComponent implements OnInit {
   private userService = inject(UserService);
   private activeRoute = inject(ActivatedRoute);
   private datePipe = inject(DatePipe);
+  private dateAdapter = inject(DateAdapter<Date>);
 
   currentUser!: string;
-  announcements$!: Observable<Announcement[]>;
-  filteredAnnouncements$!: Observable<Announcement[]>;
-  recentFilteredAnnouncements$!: Observable<Announcement[]>;
-  searchTerm: string = '';
-
+  searchText: string = '';
   priceSort: SortType = SortType.ASC;
   priceSortTypes = Object.values(SortType);
+  announcements$!: Observable<Announcement[]>;
+  filteredAnnouncements$!: Observable<Announcement[]>;
+
+  private searchSubject = new BehaviorSubject<string>('');
+  private subjectSortSubject = new BehaviorSubject<Subjects[]>([]);
+  private dateFilterSubject = new BehaviorSubject<string>('');
 
   ngOnInit(): void {
+    //change date format to dd/MM/yyyy
+    this.dateAdapter.setLocale('en-GB');
+
     this.activeRoute.queryParams.subscribe((queryParams) => {
-      console.log('queryParams chat', queryParams);
       this.currentUser = queryParams['currentUser'] ?? '';
-      console.log(this.currentUser);
 
       this.announcements$ = this.userService.getAnnouncements(this.currentUser);
-      this.filteredAnnouncements$ = this.announcements$;
-      this.recentFilteredAnnouncements$ = this.announcements$;
 
-      this.announcements$.subscribe((res) => console.log(res));
+      this.filteredAnnouncements$ = combineLatest([
+        this.searchSubject.pipe(debounceTime(300), distinctUntilChanged()),
+        this.subjectSortSubject.pipe(distinctUntilChanged()),
+        this.dateFilterSubject.pipe(distinctUntilChanged())
+      ]).pipe(
+        switchMap(([searchText, subjectSort, dateFilter]) => {
+          return this.announcements$.pipe(
+            map((announcements) => {
+              let filteredAnnouncements = announcements;
+
+              if (searchText.length > 0) {
+                filteredAnnouncements = announcements.filter((announcement) =>
+                  announcement.username
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase())
+                );
+              }
+
+              if (subjectSort.length > 0) {
+                filteredAnnouncements = filteredAnnouncements.filter(
+                  (announcement) =>
+                    subjectSort.includes(
+                      announcement.subject as unknown as Subjects
+                    )
+                );
+              }
+
+              if (dateFilter) {
+                filteredAnnouncements = filteredAnnouncements.filter(
+                  (announcement) =>
+                    this.datePipe.transform(
+                      announcement.datePosted,
+                      'd.MM.yyyy'
+                    ) === this.datePipe.transform(dateFilter, 'd.MM.yyyy')
+                );
+              }
+
+              return filteredAnnouncements;
+            })
+          );
+        })
+      );
     });
-  }
-
-  search(searchTerm: string) {
-    // if(searchTerm.length === 0) {
-    //   this.filteredAnnouncements$ = this.recentFilteredAnnouncements$;
-    // }
-
-    this.filteredAnnouncements$ = this.announcements$.pipe(
-      map((announcements) => {
-        return announcements.filter((announcement) =>
-          this.doesAnnouncementMatchSearchTerm(announcement, searchTerm)
-        );
-      })
-    );
-
-    // this.filteredAnnouncements$ = this.announcements$.pipe(
-    //   map((announcements) => {
-    //     return announcements.filter((announcement) =>
-    //       this.doesAnnouncementMatchSearchTerm(announcement, this.searchTerm)
-    //     );
-    //   })
-    // );
-  }
-
-  doesAnnouncementMatchSearchTerm(
-    announcement: Announcement,
-    searchTerm: string
-  ): boolean {
-    const lowerCaseSearchTerm = searchTerm.toLowerCase();
-
-    if (lowerCaseSearchTerm.length === 0) {
-      this.filteredAnnouncements$ = this.announcements$;
-      return false;
-    }
-    return (
-      announcement.username.toLowerCase().includes(lowerCaseSearchTerm) ||
-      this.datePipe
-        .transform(announcement.datePosted, "d.MM.yyyy 'at' H:mm : 'pl'")!
-        .toLowerCase()
-        .includes(lowerCaseSearchTerm)
-    );
-  }
-
-  sortBySubject(subjectSort: Subjects[]): void {
-    if (subjectSort.length === 0) {
-      // this.filteredAnnouncements$ = this.announcements$;
-      return;
-    }
-
-    this.filteredAnnouncements$ = this.announcements$.pipe(
-      map((announcements) => {
-        return announcements.filter((announcement) =>
-          subjectSort.includes(announcement.subject as unknown as Subjects)
-        );
-      })
-    );
   }
 
   openAddAnnouncementPage(): void {
@@ -130,16 +125,17 @@ export class AnnouncementsComponent implements OnInit {
     });
   }
 
+  search(searchText: string) {
+    this.searchSubject.next(searchText);
+  }
+
+  sortBySubject(subjectSort: Subjects[]): void {
+    this.subjectSortSubject.next(subjectSort);
+  }
+
   onDateChange(e: any) {
-    // console.log(e.value);
-    this.filteredAnnouncements$ = this.announcements$.pipe(
-      map((announcements) => {
-        return announcements.filter(
-          (announcement) =>
-            this.datePipe.transform(announcement.datePosted, 'd.MM.yyyy') ===
-            this.datePipe.transform(e.value, 'd.MM.yyyy')
-        );
-      })
+    this.dateFilterSubject.next(
+      this.datePipe.transform(e.value, 'd.MM.yyyy') || ''
     );
   }
 }
